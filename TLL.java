a80209704dfe7911058d1574ee650c15ac53b6b2
tanishq Chavan <stapdr7eam@gmail.com>
2026-01-14 12:39:09 +0530

    14-01-26

---
+++ TLL.java
@@ -0,0 +1,41 @@
+class Node {
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+
+    }
+}
+
+public class TLL {
+    public static void main(String[] args) {
+
+        Node head = new Node(10);
+        Node Second = new Node(20);
+        Node Third = new Node(30);
+        Node fourth = new Node(40);
+
+        head.next = Second;
+        head.next.next = Third;
+        head.next.next.next = fourth;
+
+        Node current = head;
+        while (current != null) {
+            System.out.println(current.data);
+            current = current.next;
+        }
+    }
+
+    static Node insertionAtBeginning(Node head, int data) {
+
+        Node newNode = new Node(data);
+        newNode.next = head;
+        head = newNode;
+
+        return head;
+    }
+
+}
+
