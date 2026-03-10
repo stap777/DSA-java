928cc2742a65cd27d5c38fa72a50daf2e8aa65a7
tanishq Chavan <stapdr7eam@gmail.com>
2026-03-10 23:21:17 +0530

    10-03-26 daily temperature

---
+++ stack/daily temperature/main.java
@@ -0,0 +1,38 @@
+import java.util.ArrayDeque;
+
+public class main {
+
+    static int [] temp(int [] arr) {
+
+        int n = arr.length;
+        int [] output = new int [n];
+
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
+
+        for (int i=0; i<n;i++) {    
+            
+            while (!stack.isEmpty() && arr[i] > arr[stack.peek()]) {
+                int index = stack.pop();
+                output[index] =  i - index;
+            } 
+            stack.push(i);
+        }
+        
+        while (stack.isEmpty()) {
+            output[stack.pop()] = 0;
+        }
+        
+        return output;
+    }
+
+    public static void main(String [] args) {
+
+        int [] arr = {73,74,75,71,69,72,76,73};
+
+        int [] result = temp(arr);
+
+        for (int x : result) {
+            System.out.println(x + "");
+        }
+    }
+}
