28aa5583632b3ab0b8b8c00d72d6c04b594b81bb
tanishq Chavan <stapdr7eam@gmail.com>
2026-05-23 15:23:19 +0530

    validPalindrome

---
+++ two_pointer/validPalindrome2/main.java
@@ -0,0 +1,39 @@
+public class main {
+
+    public static boolean validPalindrome(String s) {
+
+        int l = 0;
+        int r = s.length() - 1;
+
+        while (l < r) {
+
+            if (s.charAt(l) != s.charAt(r)) {
+
+                return isPalindrome(s, l+1, r) ||
+                isPalindrome(s,l, r-1);
+            }
+            l++;
+            r--;
+        }
+        return true;
+    }
+
+    public static boolean isPalindrome(String s, int l, int r) {
+
+        while (l < r) {
+
+            if (s.charAt(l) != s.charAt(r)) {
+                return false;
+            }
+            l++;
+            r--;
+        }
+        return true;
+
+    } 
+    
+    public static void main(String [] args) {
+        String s = "abca";
+        System.out.println(validPalindrome(s));
+    }
+}
